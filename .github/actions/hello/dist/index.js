bef9f5b2c3bc57b3b62846ecce6843743670579d
jshernandez05 <jshernandez05@gmail.com>
2023-04-22 09:06:31 -0700

    docs(dist): recompliled dist

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -9847,13 +9847,22 @@ const github = __nccwpck_require__(4637);
 try {
   //   throw new Error("some error message");
 
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
+
   const name = core.getInput("who-to-greet");
+  core.setSecret(name);
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("Logging github object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup("Logging github object");
+
+  core.exportVariable("HELLO", "hello");
 } catch (error) {
   core.setFailed(error.message);
 }
